c24c8fce158df23dacf94294f4bed5b0d078d9cf
Yoann Chaudet <yoannchaudet@github.com>
2023-05-02 16:44:18 -0700

    wip

--- create-deployments.sh
+++ create-deployments.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env pwsh
+
+# Get ref
+$ref=(& git rev-parse HEAD)
 
 # Create some manual deployments
+& gh api `
+  --method POST `
+  -H "Accept: application/vnd.github+json" `
+  -H "X-GitHub-Api-Version: 2022-11-28" `
+  /repos/yoannchaudet/deployments/deployments `
+  -f ref="$ref" `
+  -f description='Deploy via REST API' `
+  -f environment='qa'
